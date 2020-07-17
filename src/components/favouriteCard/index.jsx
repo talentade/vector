393302e3c1b4633648a7d485e5963735cd9ca03e
talentade <adeoyetalent@gmail.com>
2020-07-17 08:19:11 +0100

    July 17

--- src/components/favouriteCard/index.jsx
+++ src/components/favouriteCard/index.jsx
@@ -21,11 +21,11 @@ const FavouriteCard = ({ direction, color, pair, price }) => (
       <p>{price}</p>
       {direction == "up" ? (
         <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M8.20123 13.1172L9.7183 13.1172V4.63187H1.23302L1.23302 6.14894L8.20123 6.14894L8.20123 13.1172Z" fill={color.length ? '#FF4848' : '#03CF9E'}/>
+        <path d="M8.20123 13.1172L9.7183 13.1172V4.63187H1.23302L1.23302 6.14894L8.20123 6.14894L8.20123 13.1172Z" fill={color ? (color.length ? '#FF4848' : '#03CF9E') : '#03CF9E'}/>
         </svg>
       ) : (
         <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1.23392 8.11603L1.23392 9.63309L9.7192 9.63309L9.7192 1.14781L8.20214 1.14781L8.20214 8.11603L1.23392 8.11603Z" fill={color.length ? '#FF4848' : '#03CF9E'}/>
+        <path d="M1.23392 8.11603L1.23392 9.63309L9.7192 9.63309L9.7192 1.14781L8.20214 1.14781L8.20214 8.11603L1.23392 8.11603Z" fill={color ? (color.length ? '#FF4848' : '#03CF9E') : '#03CF9E'}/>
         </svg>
       )}
       <img src={Comment} alt="" />
