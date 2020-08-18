1d62d83e2e221605ef74978c7934f9283578426b
talentade <adeoyetalent@gmail.com>
2020-08-18 12:04:42 +0100

    Third live push

--- src/services/app.js
+++ src/services/app.js
@@ -76,7 +76,7 @@ export default {
   hostURL: (url, type = 0) => {
     let live = true;
     if(type > 0) {
-      return live ? "ws://avariz-server-one.herokuapp.com" : "ws://localhost:3003";
+      return live ? "wss://avariz-server-one.herokuapp.com" : "ws://localhost:3003";
     } else {
       return live ? "https://avariz-server-one.herokuapp.com/"+url : "http://localhost:3003/"+url;
     }
