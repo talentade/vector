b4ae485124f5cd1636d7c11d17cf67d0087cba5d
talentade <adeoyetalent@gmail.com>
2020-12-15 11:14:17 +0100

    Notifications 2

--- src/components/notifications/index.jsx
+++ src/components/notifications/index.jsx
@@ -5,6 +5,7 @@ import dbn from '../../themes/images/double-next.png';
 import ppl from '../../themes/images/paper-plane.png';
 import server from '../../services/server';
 import app from '../../services/app';
+import moment from 'moment';
 import $ from 'jquery';
 
 import "./index.scss";
@@ -50,13 +51,21 @@ class NotificationBox extends Component {
     return (
       this.props.show ?
       <div className={"notification-dropdown"+(this.state.mobile ? ' mobile' : '')+(app.isAdmin() ? ' admin' : '')}>
+        <div className="nhead">
+          <h6>Notifications</h6>
+          <span className="clear_all">Clear all</span>
+        </div>
         <div className="section2">
           <ul id="notificationList">
           {
             this.state.notifications.map((n) => (
-              <li className={n.flag ? "" : "new"} onClick={() => this.gotoN(n.goto)} style={n.goto.length ? {cursor: "ponter"} : null}>{n.notice}</li>
+              <li className={n.flag ? "" : "new"} onClick={() => this.gotoN(n.goto)} style={n.goto.length ? {cursor: "ponter"} : null}>
+                {n.notice}
+                <span>{moment(n.create_time).calendar()}</span>
+              </li>
             ))
           }
+          {this.state.notifications.length ? null : <li>No new notification</li>}
           </ul>
         </div>
       </div> : null
